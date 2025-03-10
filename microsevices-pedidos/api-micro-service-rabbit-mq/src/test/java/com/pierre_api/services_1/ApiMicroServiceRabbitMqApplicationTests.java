ef5c9f1cf31da8024c11bab53fe27c3f5aa96a7b
pierre enoc <pierre_enoc@yahoo.fr>
2025-03-10 17:34:28 -0300

    start project

---
+++ microsevices-pedidos/api-micro-service-rabbit-mq/src/test/java/com/pierre_api/services_1/ApiMicroServiceRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pierre_api.services_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMicroServiceRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
