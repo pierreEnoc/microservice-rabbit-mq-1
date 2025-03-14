29ce936dfa5e6cbe05712e186dc46d8803a5059d
pierre enoc <pierre_enoc@yahoo.fr>
2025-03-14 18:46:51 -0300

    project processor created

---
+++ microsevices-pedidos/processador/src/main/java/com/pierre/api/pedidos/processador/ProcessadorApplication.java
@@ -0,0 +1,13 @@
+package com.pierre.api.pedidos.processador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessadorApplication.class, args);
+	}
+
+}
